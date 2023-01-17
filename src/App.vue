25f30cf0e545649079d813de9db103ed8c80b190
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-01-17 18:48:02 +0100

    Added App component template and style tag content

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,29 @@
 </script>
 
 <template>
-    <p>Hello World</p>
+    <h1 class="title">Text Shadow Page</h1>
+
+    <div class="container">
+        <p v-for="n in 3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum itaque, soluta vero, necessitatibus cumque voluptates veniam commodi hic ab fuga, quia vel ea nihil sit labore nisi odio deleniti enim provident tenetur voluptas! Nisi optio distinctio animi placeat aut ex obcaecati, quia ad non laboriosam voluptate eum ipsam soluta aspernatur.</p>
+    </div>
 </template>
 
 <style scoped>
-    p {
-        color: red;
+    .title {
+        font-size: 3rem;
+        text-align: center;
+        user-select: none;
+    }
+    .container {
+        margin-top: 2rem;
+        width: 100%;
+        display: flex;
+        gap: 2rem;
+        justify-content: space-around;
+        user-select: none;
+    }
+
+    .container > p {
+        padding: 4rem;
     }
 </style>
